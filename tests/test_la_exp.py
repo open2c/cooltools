58bf2a371f2faf6097abd30cf47fd2b03d6baef7
sergpolly <sergpolly@gmail.com>
2018-01-25 19:19:50 -0500

    more tests added

--- tests/test_la_exp.py
+++ tests/test_la_exp.py
@@ -14,11 +14,10 @@ import sys
 sys.path.append("../")
 
 # try importing stuff from loopify:
-from loopify import get_adjusted_expected
-# ,
-                    # get_adjusted_expected_tile,
-                    # get_adjusted_expected_tile_some_nans,
-                    # get_adjusted_expected_some_nans
+from loopify import get_adjusted_expected,
+                    get_adjusted_expected_tile,
+                    get_adjusted_expected_tile_some_nans,
+                    get_adjusted_expected_some_nans
 
 
 testdir = op.realpath(op.dirname(__file__))
@@ -57,7 +56,8 @@ mock_res = pd.read_csv(mock_result)
 
 
 
-def test_adjusted_expected():
+def test_adjusted_expected_reference():
+    print("Running reference la_exp test")
     # first, generate that locally-adjusted expected:
     # Ed_raw, mask_ndx, Cobs, Cexp, NN = 
     res = get_adjusted_expected(observed=mock_M_ice,
@@ -84,18 +84,152 @@ def test_adjusted_expected():
 
 
 
+def test_adjusted_expected_tile():
+    print("Running tile la_exp test")
+    # first, generate that locally-adjusted expected:
+    # Ed_raw, mask_ndx, Cobs, Cexp, NN = 
+    res = get_adjusted_expected_tile(
+                         origin=(0,0),
+                         observed=mock_M_ice,
+                         expected=mock_E_ice,
+                         ice_weight=mock_v_ice,
+                         kernels=(kernel,),
+                         b=b,
+                         band=2e+6)
+    # ACTUAL TESTS:
+    # integer part of DataFrame must equals exactly:
+    assert (
+        res[['row','col']].equals(
+            mock_res[['row','col']])
+        )
+    # compare floating point part separately:
+    assert (
+        np.isclose(
+            res[['expected','observed']],
+            mock_res[['la_expected','observed']],
+            equal_nan=True).all()
+        )
+
+
+
+
+def test_adjusted_expected_tile_some_nans():
+    print("Running tile some nans la_exp test")
+    # first, generate that locally-adjusted expected:
+    # Ed_raw, mask_ndx, Cobs, Cexp, NN = 
+    res = get_adjusted_expected_tile_some_nans(
+                         origin=(0,0),
+                         observed=mock_M_ice,
+                         expected=mock_E_ice,
+                         ice_weight=mock_v_ice,
+                         kernels=(kernel,),
+                         b=b,
+                         band=2e+6,
+                         nan_threshold=1)
+    # ACTUAL TESTS:
+    # integer part of DataFrame must equals exactly:
+    assert (
+        res[['row','col']].equals(
+            mock_res[['row','col']])
+        )
+    # compare floating point part separately:
+    assert (
+        np.isclose(
+            res[['expected','observed']],
+            mock_res[['la_expected','observed']],
+            equal_nan=True).all()
+        )
+    #
+    # try recovering NaNs ...
+    #
+    #
+    res = get_adjusted_expected_tile_some_nans(
+                     origin=(0,0),
+                     observed=mock_M_ice,
+                     expected=mock_E_ice,
+                     ice_weight=mock_v_ice,
+                     kernels=(kernel,),
+                     b=b,
+                     band=2e+6,
+                     nan_threshold=2)
+    # now we can only guess the size:
+    assert (res.size > mock_res.size)
+
+
+
+
+
+def test_adjusted_expected_some_nans():
+    print("Running some nans la_exp test")
+    # first, generate that locally-adjusted expected:
+    # Ed_raw, mask_ndx, Cobs, Cexp, NN = 
+    res = get_adjusted_expected_some_nans(
+                         observed=mock_M_ice,
+                         expected=mock_E_ice,
+                         ice_weight=mock_v_ice,
+                         kernels=(kernel,),
+                         b=b,
+                         band=2e+6,
+                         nan_threshold=1)
+    # ACTUAL TESTS:
+    # integer part of DataFrame must equals exactly:
+    assert (
+        res[['row','col']].equals(
+            mock_res[['row','col']])
+        )
+    # compare floating point part separately:
+    assert (
+        np.isclose(
+            res[['expected','observed']],
+            mock_res[['la_expected','observed']],
+            equal_nan=True).all()
+        )
+    #
+    # try recovering NaNs ...
+    #
+    #
+    res = get_adjusted_expected_some_nans(
+                     observed=mock_M_ice,
+                     expected=mock_E_ice,
+                     ice_weight=mock_v_ice,
+                     kernels=(kernel,),
+                     b=b,
+                     band=2e+6,
+                     nan_threshold=2)
+    # now we can only guess the size:
+    assert (res.size > mock_res.size)
 
 
 
 
 
 
+# def get_adjusted_expected_tile(origin,
+#                                observed,
+#                                expected,
+#                                ice_weight,
+#                                kernels,
+#                                b,
+#                                band=2e+6)
 
 
+# def get_adjusted_expected_tile_some_nans(origin,
+#                                          observed,
+#                                          expected,
+#                                          ice_weight,
+#                                          kernels,
+#                                          b,
+#                                          band=2e+6,
+#                                          nan_threshold=2)
 
 
-
-
+# def get_adjusted_expected_some_nans(observed,
+#                                     expected,
+#                                     ice_weight,
+#                                     kernels,
+#                                     b,
+#                                     band=2e+6,
+#                                     nan_threshold=2):
 
 
 
