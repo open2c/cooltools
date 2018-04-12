fb210f1fd6eff64ebd5bf24f98b1cfc7f67d78ca
sergpolly <sergpolly@gmail.com>
2018-04-12 16:26:27 -0400

    added test for new square-tile-around-diag-case

--- tests/test_chunking.py
+++ tests/test_chunking.py
@@ -196,4 +196,93 @@ def test_adjusted_expected_tile_some_nans_and_square_tiling():
 
 
 
+def test_adjusted_expected_tile_some_nans_and_square_tiling_diag_band():
+    print("Running tile some nans la_exp test + square tiling + diag band")
+    # # Essentially, testing this function:
+    # def chrom_chunk_generator_s(chroms, w, band):
+    #     for chrom in chroms:
+    #         chr_start, chr_stop = the_c.extent(chrom)
+    #         for tilei, tilej in square_matrix_tiling(chr_start, chr_stop, tile_size, w):
+    #             # check if a given tile intersects with 
+    #             # with the diagonal band of interest ...
+    #             diag_from = tilej[0] - tilei[1]
+    #             diag_to   = tilej[1] - tilei[0]
+    #             #
+    #             band_from = 0
+    #             band_to   = band
+    #             # we are using this >2w trick to exclude
+    #             # tiles from the lower triangle from calculations ...
+    #             if (min(band_to,diag_to) - max(band_from,diag_from)) > 2*w:
+    #                 yield chrom, tilei, tilej
+    # first, generate that locally-adjusted expected:
+    nnans = 1
+    band_idx = int(band/b)
+    res_df = pd.DataFrame([])
+    for tilei, tilej in loopify.square_matrix_tiling(start, stop, tile_size=40, edge=w, square=False):
+        # check if a given tile intersects with 
+        # with the diagonal band of interest ...
+        diag_from = tilej[0] - tilei[1]
+        diag_to   = tilej[1] - tilei[0]
+        #
+        band_from = 0
+        band_to   = band_idx
+        # we are using this >2w trick to exclude
+        # tiles from the lower triangle from calculations ...
+        if (min(band_to,diag_to) - max(band_from,diag_from)) > 2*w:
+            # define origin:
+            origin = (tilei[0], tilej[0])
+            # RAW observed matrix slice:
+            observed = mock_M_raw[slice(*tilei),slice(*tilej)]
+            # trying new expected function:
+            expected = mock_exp[slice(*tilei),slice(*tilej)]
+            # for diagonal chuynking/tiling tilei==tilej:
+            ice_weight_i = mock_v_ice[slice(*tilei)]
+            ice_weight_j = mock_v_ice[slice(*tilej)]
+            # that's the main working function from loopify:
+            res = loopify.get_adjusted_expected_tile_some_nans(origin = origin,
+                                                     observed = observed,
+                                                     expected = expected,
+                                                     bal_weight = (ice_weight_i, ice_weight_j),
+                                                     kernels = {"donut":kernel,
+                                                             "footprint":np.ones_like(kernel)},
+                                                     # nan_threshold=1,
+                                                     verbose=False)
+            is_inside_band = (res["bin1_id"] > (res["bin2_id"]-band_idx))
+            # new style, selecting good guys:
+            does_comply_nans = (res["la_exp."+"footprint"+".nnans"] < nnans)
+            # so, select inside band and nNaNs compliant results and append:
+            res_df = res_df.append(
+                                res[is_inside_band & does_comply_nans],
+                                ignore_index=True)
+
+    # sort and reset index, there shouldn't be any duplicates now:
+    res_df = res_df \
+                .sort_values(by=['bin1_id','bin2_id']) \
+                .reset_index(drop=True)
+
+    # prepare mock_data for comparison:
+    # apparently sorting is needed in this case:
+    mock_res_sorted = mock_res.sort_values(by=['bin1_id','bin2_id']).reset_index(drop=True)
+
+    # ACTUAL TESTS:
+    # integer part of DataFrame must equals exactly:
+    assert (
+        res_df[['bin1_id','bin2_id']].equals(
+            mock_res_sorted[['bin1_id','bin2_id']])
+        )
+    # compare floating point part separately:
+    assert (
+        np.isclose(
+            res_df["la_exp."+"donut"+".value"],
+            mock_res_sorted['la_expected'],
+            equal_nan=True).all()
+        )
+
+
+
+
+
+
+
+
 
