ad6bb5b787b4b959ff24c71122fc6f4d1a7e7ff9
Nezar <nabdennur@gmail.com>
2018-05-06 15:41:51 -0400

    Add top-level cli debugging and verbosity options

--- cooltools/cli/__init__.py
+++ cooltools/cli/__init__.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import division, print_function
 import click
+import sys
 from .. import __version__
 
 
@@ -15,8 +16,35 @@ CONTEXT_SETTINGS = {
 
 @click.version_option(version=__version__)
 @click.group(context_settings=CONTEXT_SETTINGS)
-def cli():
-    pass
+@click.option(
+    '--debug/--no-debug', 
+    help="Verbose logging", 
+    default=False)
+@click.option(
+    '-pm', '--post-mortem', 
+    help="Post mortem debugging", 
+    is_flag=True,
+    default=False)
+def cli(debug, post_mortem):
+    """
+    Type -h or --help after any subcommand for more information.
+
+    """
+    if debug:
+    	pass
+        #logger.setLevel(logging.DEBUG)
+
+    if post_mortem:
+        import traceback
+        try:
+            import ipdb as pdb
+        except ImportError:
+            import pdb
+        def _excepthook(exc_type, value, tb):
+            traceback.print_exception(exc_type, value, tb)
+            print()
+            pdb.pm()
+        sys.excepthook = _excepthook
 
 
 from . import (
