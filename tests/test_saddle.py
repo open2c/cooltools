eac5af2a5ae444937c7e3a40a616516c8979b75e
Nezar <nabdennur@gmail.com>
2018-05-06 18:38:27 -0400

    Update tests

---
+++ tests/test_saddle.py
@@ -0,0 +1,27 @@
+import os.path as op
+import numpy as np
+import pandas as pd
+
+import bioframe
+import cooler
+import cooltools.saddle
+
+# chromsizes = bioframe.fetch_chromsizes('mm9')
+# chromosomes = list(chromsizes.index)
+# supports = [(chrom, 0, chromsizes[chrom]) for chrom in chromosomes]
+
+
+# def test_digitize_track(request):
+#     pass
+
+
+# def test_make_saddle(request):
+#     pass
+
+
+# def test_saddleplot(request):
+#     pass
+
+
+# def test_saddlestrength(request):
+#     pass
