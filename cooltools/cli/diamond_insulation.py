ea3d49e06a877d2a4b0693662136d7aee58f6303
Nezar <nabdennur@gmail.com>
2018-06-26 20:23:58 -0400

    Add balancing option to diamond insulation command

--- cooltools/cli/diamond_insulation.py
+++ cooltools/cli/diamond_insulation.py
@@ -15,27 +15,29 @@ from .. import insulation
     'window',
     nargs=-1,
     metavar="WINDOW",
-    type=int,
-    )
+    type=int)
 @click.option(
     '--min-dist-bad-bin', 
     help='The minimal allowed distance to a bad bin. Do not calculate insulation scores '
          'for bins having a bad bin closer than this distance.',
     type=int,
     default=2,
-    show_default=True,
-    )
+    show_default=True)
+@click.option(
+    '--balance/--no-balance',
+    default=True)
+@click.option(
+    '--balance-weight', '-b',
+    type=str,
+    default='weight')
 @click.option(
     '--ignore-diags', 
     help='The number of diagonals to ignore. By default, equals'
         ' the number of diagonals ignored during IC balancing.',
     type=int,
     default=None,
-    show_default=True,
-    )
-
-
-def diamond_insulation(cool_path, window, min_dist_bad_bin, ignore_diags):
+    show_default=True)
+def diamond_insulation(cool_path, window, balance, balance_weight, min_dist_bad_bin, ignore_diags):
     """
     Calculate the diamond insulation scores and call insulating boundaries.
     
@@ -44,9 +46,12 @@ def diamond_insulation(cool_path, window, min_dist_bad_bin, ignore_diags):
     WINDOW : The window size for the insulation score calculations.
     """
 
-    c = cooler.Cooler(cool_path)
+    clr = cooler.Cooler(cool_path)
     ins_table = insulation.find_insulating_boundaries(
-        c, window_bp = window, min_dist_bad_bin = min_dist_bad_bin,
+        clr, 
+        window_bp=window, 
+        balance=balance_weight if balance else False,
+        min_dist_bad_bin=min_dist_bad_bin,
         ignore_diags=ignore_diags)
 
     print(ins_table.to_csv(sep='\t', index=False, na_rep='nan'))
