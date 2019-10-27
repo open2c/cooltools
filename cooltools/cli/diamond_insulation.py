b6028fd4c8bb28e290d434686768f86e3659a818
Nezar <nabdennur@gmail.com>
2019-10-27 16:49:44 -0400

    Add tunbale chunksize option to insulation

--- cooltools/cli/diamond_insulation.py
+++ cooltools/cli/diamond_insulation.py
@@ -5,6 +5,7 @@ import cooler
 from . import cli
 from .. import insulation
 
+
 @cli.command()
 @click.argument(
     "in_path",
@@ -19,7 +20,7 @@ from .. import insulation
 @click.option(
     '--ignore-diags',
     help='The number of diagonals to ignore. By default, equals'
-        ' the number of diagonals ignored during IC balancing.',
+         ' the number of diagonals ignored during IC balancing.',
     type=int,
     default=None,
     show_default=True)
@@ -47,19 +48,24 @@ from .. import insulation
          'to the output table.',
     is_flag=True)
 @click.option(
+    '--chunksize',
+    help='',
+    type=int,
+    default=20000000,
+    show_default=True)
+@click.option(
     '--verbose',
     help='Report real-time progress.',
     is_flag=True)
-
-
-def diamond_insulation(in_path, window, ignore_diags, min_frac_valid_pixels, 
-                       min_dist_bad_bin, window_pixels, append_raw_scores, verbose):
+def diamond_insulation(in_path, window, ignore_diags, min_frac_valid_pixels,
+                       min_dist_bad_bin, window_pixels, append_raw_scores,
+                       chunksize, verbose):
     """
     Calculate the diamond insulation scores and call insulating boundaries.
 
     IN_PATH : The paths to a .cool file with a balanced Hi-C map.
 
-    WINDOW : The window size for the insulation score calculations. 
+    WINDOW : The window size for the insulation score calculations.
              Multiple space-separated values can be provided.
              By default, the window size must be provided in units of bp.
              When the flag --window-pixels is set, the window sizes must
@@ -69,22 +75,19 @@ def diamond_insulation(in_path, window, ignore_diags, min_frac_valid_pixels,
     clr = cooler.Cooler(in_path)
     if window_pixels:
         window = [win*clr.info['bin-size'] for win in window]
-    
-    
+
     ins_table = insulation.calculate_insulation_score(
         clr,
         window_bp=window,
         ignore_diags=ignore_diags,
         append_raw_scores=append_raw_scores,
+        chunksize=chunksize,
         verbose=verbose)
-    
-    
+
     ins_table = insulation.find_boundaries(
         ins_table,
         min_frac_valid_pixels=min_frac_valid_pixels,
         min_dist_bad_bin=min_dist_bad_bin,
     )
 
-
     print(ins_table.to_csv(sep='\t', index=False, na_rep='nan'))
-
