285136767922adf9c45ad53cbbe6f3c172fb0957
Ilya Flyamer <flyamer@gmail.com>
2020-04-14 20:49:42 +0100

    Allow regions in expected CLI (#146)
    * Make saddle strength work with NaNs
    
    * Allow regions in CLI for expected
    
    * Fix region_names without regions

--- cooltools/cli/compute_expected.py
+++ cooltools/cli/compute_expected.py
@@ -5,6 +5,7 @@ from .. import expected
 
 import click
 from . import cli
+from . import util
 
 # might be relevant to us ...
 # https://stackoverflow.com/questions/46577535/how-can-i-run-a-dask-distributed-local-cluster-from-the-command-line
@@ -12,12 +13,7 @@ from . import cli
 
 
 @cli.command()
-@click.argument(
-    "cool_path",
-    metavar="COOL_PATH",
-    type=str,
-    nargs=1
-)
+@click.argument("cool_path", metavar="COOL_PATH", type=str, nargs=1)
 @click.option(
     "--nproc",
     "-p",
@@ -52,22 +48,22 @@ from . import cli
     "--contact-type",
     "-t",
     help="compute expected for cis or trans region of a Hi-C map."
-         "Ignored when genomic-regions are provided",
+    "Ignored when genomic-regions are provided",
     type=click.Choice(["cis", "trans"]),
     default="cis",
     show_default=True,
 )
 @click.option(
-    "--genomic-regions",
-    help="Path to a BED or BEDPE file containing genomic regions "
-         "for which expected will be calculated. [Not Implemented]",
+    "--regions",
+    help="Path to a BED file containing genomic regions "
+    "for which expected will be calculated.",
     type=click.Path(exists=True),
     required=False,
 )
 @click.option(
     "--balance/--no-balance",
     help="Apply balancing weights to data before calculating expected."
-         "Bins masked in the balancing weights are ignored from calcualtions.",
+    "Bins masked in the balancing weights are ignored from calcualtions.",
     is_flag=True,
     default=True,
     show_default=True,
@@ -82,8 +78,8 @@ from . import cli
 @click.option(
     "--blacklist",
     help="Path to a 3-column BED file containing genomic regions to mask "
-         "out during calculation of expected. Overwrites inference of "
-         "'bad' regions from balancing weights. [Not Implemented]",
+    "out during calculation of expected. Overwrites inference of "
+    "'bad' regions from balancing weights. [Not Implemented]",
     type=click.Path(exists=True),
     required=False,
 )
@@ -101,7 +97,7 @@ def compute_expected(
     output,
     hdf,
     contact_type,
-    genomic_regions,
+    regions,
     balance,
     weight_name,
     blacklist,
@@ -118,24 +114,41 @@ def compute_expected(
 
     """
 
-    if genomic_regions is not None:
-        raise NotImplementedError(
-            "Custom genomic regions for calculation of expected are not implemented in CLI,"
-            "use `cooltools.expected.blocksum/diagsum` functions."
-            )
+    # if regions is not None:
+    #     raise NotImplementedError(
+    #         "Custom genomic regions for calculation of expected are not implemented in CLI,"
+    #         "use `cooltools.expected.blocksum/diagsum` functions."
+    #         )
 
     if blacklist is not None:
         raise NotImplementedError(
             "Custom genomic regions for masking from calculation of expected"
             "are not implemented."
-            )
+        )
         # use blacklist-ing from cooler balance module
         # https://github.com/mirnylab/cooler/blob/843dadca5ef58e3b794dbaf23430082c9a634532/cooler/cli/balance.py#L175
 
-
-
     clr = cooler.Cooler(cool_path)
-    supports = [(chrom, 0, clr.chromsizes[chrom]) for chrom in clr.chromnames]
+    if regions is None:
+        regions = [(chrom, 0, clr.chromsizes[chrom]) for chrom in clr.chromnames]
+        region_names = [chrom for chrom in clr.chromnames]
+    else:
+        regions_buf, names = util.sniff_for_header(regions)
+        if names is not None:
+            regions = pd.read_csv(regions_buf, skiprows=1, sep="\t", header=None)
+        else:
+            regions = pd.read_csv(regions_buf, sep="\t", header=None)
+        if regions.shape[1] not in (3, 4):
+            raise ValueError(
+                "The region file does not have three or four tab-delimited columns."
+                "We expect a bed file with columns chrom, start, end, and optional name"
+            )
+        if regions.shape[1] == 4:
+            region_names = regions[:, 3]
+            regions = regions[:, :3]
+        else:
+            region_names = list(regions.apply(lambda x: "{}:{}-{}".format(*x), axis=1))
+        regions = [tuple(row)[:3] for i, row in regions.iterrows()]
 
     # define transofrms - balanced and raw ('count') for now
     if balance:
@@ -159,38 +172,42 @@ def compute_expected(
         if contact_type == "cis":
             tables = expected.diagsum(
                 clr,
-                supports,
+                regions,
                 transforms=transforms,
                 weight_name=weight_name,
                 bad_bins=None,
                 chunksize=chunksize,
                 ignore_diags=ignore_diags,
-                map=map_
+                map=map_,
             )
             result = pd.concat(
-                [tables[support] for support in supports],
-                keys=[support[0] for support in supports],
-                names=["chrom"],
+                [tables[region] for region in regions],
+                keys=[name for name in region_names],
+                names=["region"],
             )
             result = result.reset_index()
 
         elif contact_type == "trans":
             records = expected.blocksum_pairwise(
                 clr,
-                supports,
+                regions,
                 transforms=transforms,
                 weight_name=weight_name,
                 bad_bins=None,
                 chunksize=chunksize,
-                map=map_
+                map=map_,
             )
             result = pd.DataFrame(
                 [
-                    {"chrom1": s1[0], "chrom2": s2[0], **rec}
-                    for (s1, s2), rec in records.items()
+                    {
+                        "region1": region_names[regions.index(r1)],
+                        "region2": region_names[regions.index(r2)],
+                        **rec,
+                    }
+                    for (r1, r2), rec in records.items()
                 ],
-                columns=["chrom1", "chrom2", "n_valid", "count.sum"] + \
-                        [ k+".sum" for k in transforms.keys() ]
+                columns=["region1", "region2", "n_valid", "count.sum"]
+                + [k + ".sum" for k in transforms.keys()],
             )
     finally:
         if nproc > 1:
