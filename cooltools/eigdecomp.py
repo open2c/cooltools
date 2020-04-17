a8ed7763a3ef3f0552a201b72bad4e8954763bcc
Sameer Abraham <sameer.abraham0@gmail.com>
2020-04-17 17:09:43 -0400

    Change to new bioframe function names (#149)
    * updated bioframe.slice_bedframe to bioframe.bedslice in line 337
    
    * updated bioframe.bisect_bedframe to bioframe.bedbisect in line 356

--- cooltools/eigdecomp.py
+++ cooltools/eigdecomp.py
@@ -334,7 +334,7 @@ def cooler_cis_eig(
                 'No column "{}" in the bin table'.format(phasing_track_col)
             )
         phasing_track = (
-            bioframe.slice_bedframe(bins, region)[phasing_track_col].values
+            bioframe.bedslice(bins, region)[phasing_track_col].values
             if phasing_track_col
             else None
         )
@@ -353,7 +353,7 @@ def cooler_cis_eig(
     eigvals_per_reg, eigvecs_per_reg = zip(*map(_each, regions))
 
     for region, eigvecs in zip(regions, eigvecs_per_reg):
-        lo, hi = bioframe.bisect_bedframe(bins, region)
+        lo, hi = bioframe.bedbisect(bins, region)
         for i, eigvec in enumerate(eigvecs):
             eigvec_table.iloc[
                 lo:hi, eigvec_table.columns.get_loc("E" + str(i + 1))
