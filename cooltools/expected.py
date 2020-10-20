c2969e2e4bec9b65b53adeb592f27b10bae6ea42
sergpolly <sergpolly@gmail.com>
2020-10-20 11:55:37 -0400

    added csv validation and custom exp_summary_name

--- cooltools/expected.py
+++ cooltools/expected.py
@@ -922,6 +922,7 @@ def blocksum_asymm(
 
 def logbin_expected(
     exp,
+    exp_summary_name="balanced.sum",
     bins_per_order_magnitude=10,
     bin_layout="fixed",
     der_smooth_function_by_reg=lambda x: numutils.robust_gauss_filter(x, 2),
@@ -935,6 +936,10 @@ def logbin_expected(
     ----------
     exp : DataFrame
         DataFrame produced by diagsum
+
+    exp_summary_name : str (optional)
+        Name of the column of exp-DataFrame to use as a diagonal summary.
+        Default is "balanced.sum".
 
     bins_per_order_magnitude : int (optional)
         How many bins per order of magnitude. Default of 10 has a ratio of
@@ -1049,7 +1054,7 @@ def logbin_expected(
     """
     from cooltools.lib.numutils import logbins
 
-    exp = exp[~pd.isna(exp["balanced.sum"])]
+    exp = exp[~pd.isna(exp[exp_summary_name])]
     exp["x"] = exp.pop("diag")
     diagmax = exp["x"].max()
 
@@ -1078,7 +1083,7 @@ def logbin_expected(
 
     byRegExp = byRegExp.reset_index()
     byRegExp = byRegExp[byRegExp["n_valid"] > min_nvalid]  # filtering by n_valid
-    byRegExp["Pc"] = byRegExp["balanced.sum"] / byRegExp["n_valid"]
+    byRegExp["Pc"] = byRegExp[exp_summary_name] / byRegExp["n_valid"]
     byRegExp = byRegExp[byRegExp["Pc"] > 0]  # drop bins with 0 counts
     if min_count:
         if "count.sum" in byRegExp:
