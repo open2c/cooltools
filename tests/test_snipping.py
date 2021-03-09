3c19b5cfef28c29b09a6ea8a8f7215837964554c
Aleksandra Galitsyna <agalitzina@gmail.com>
2021-03-09 16:28:28 -0500

    formatting imporoved

--- tests/test_snipping.py
+++ tests/test_snipping.py
@@ -4,6 +4,7 @@ import os.path as op
 import cooltools.snipping
 import numpy as np
 
+
 def test_snipper(request):
     """
     Test the snipping on matrix:
@@ -14,37 +15,27 @@ def test_snipper(request):
     regions = [(chrom, 0, clr.chromsizes[chrom]) for chrom in clr.chromnames]
 
     # Example region with windows, two regions from annotated genomic regions:
-    windows = cooltools.snipping.make_bin_aligned_windows(1_000_000,
-                                                        ['chr10', 'chr10'],
-                                                        [10_000_000, 20_000_000],
-                                                        flank_bp=2_000_000)
-    
+    windows = cooltools.snipping.make_bin_aligned_windows(
+        1_000_000, ["chr10", "chr10"], [10_000_000, 20_000_000], flank_bp=2_000_000
+    )
+
     windows = cooltools.snipping.assign_regions(windows, regions).reset_index(drop=True)
 
     snipper = cooltools.snipping.CoolerSnipper(clr)
-    stack = cooltools.snipping.pileup(
-            windows,
-            snipper.select,
-            snipper.snip,
-            map=map)
+    stack = cooltools.snipping.pileup(windows, snipper.select, snipper.snip, map=map)
 
     # Check that the size of snips is OK and there are two of them:
     assert stack.shape == (5, 5, 2)
 
     # Example region with windows, second window comes from unannotated genomic region:
-    windows = cooltools.snipping.make_bin_aligned_windows(1_000_000,
-                                                        ['chr10', 'chr10'],
-                                                        [10_000_000, 150_000_000],
-                                                        flank_bp=2_000_000)
-    
+    windows = cooltools.snipping.make_bin_aligned_windows(
+        1_000_000, ["chr10", "chr10"], [10_000_000, 150_000_000], flank_bp=2_000_000
+    )
+
     windows = cooltools.snipping.assign_regions(windows, regions).reset_index(drop=True)
 
     snipper = cooltools.snipping.CoolerSnipper(clr)
-    stack = cooltools.snipping.pileup(
-            windows,
-            snipper.select,
-            snipper.snip,
-            map=map)
+    stack = cooltools.snipping.pileup(windows, snipper.select, snipper.snip, map=map)
 
     assert stack.shape == (5, 5, 2)
     assert np.all(np.isfinite(stack[:, :, 0]))
