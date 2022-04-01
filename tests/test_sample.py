2fc6d47b346a68d3d8bb6abfb917b2ba0e033182
Phlya <flyamer@gmail.com>
2022-04-01 14:26:55 +0200

    Add 10Mb cooler, use in exact sample test

--- tests/test_sample.py
+++ tests/test_sample.py
@@ -33,27 +33,30 @@ def test_sample(request):
     testing.assert_allclose(clr_result.info["sum"], 200000000, rtol=1e-3)
 
 
-# Exact sampling is very slow! So commented out
-# cooltools.api.sample.sample(
-#     clr,
-#     op.join(request.fspath.dirname, "data/CN.mm9.1000kb.test_sampled.cool"),
-#     frac=0.5,
-#     exact=True,
-# )
-# clr_result = cooler.Cooler(
-#     op.join(request.fspath.dirname, "data/CN.mm9.1000kb.test_sampled.cool")
-# )
-# # Test that result matches expectation exactly
-# testing.assert_equal(clr_result.info["sum"], round(clr.info["sum"] * 0.5))
+def test_sample_exact(request):
+    # Exact sampling is very slow! So commented out
+    clr = cooler.Cooler(op.join(request.fspath.dirname, "data/CN.mm9.10000kb.cool"))
 
-# cooltools.api.sample.sample(
-#     clr,
-#     op.join(request.fspath.dirname, "data/CN.mm9.1000kb.test_sampled.cool"),
-#     count=200000000,
-#     exact=True,
-# )
-# clr_result = cooler.Cooler(
-#     op.join(request.fspath.dirname, "data/CN.mm9.1000kb.test_sampled.cool")
-# )
-# # Test that result matches expectation exactly
-# testing.assert_equal(clr_result.info["sum"], 200000000)
+    cooltools.api.sample.sample(
+        clr,
+        op.join(request.fspath.dirname, "data/CN.mm9.10000kb.test_sampled.cool"),
+        frac=0.5,
+        exact=True,
+    )
+    clr_result = cooler.Cooler(
+        op.join(request.fspath.dirname, "data/CN.mm9.10000kb.test_sampled.cool")
+    )
+    # Test that result matches expectation exactly
+    testing.assert_equal(clr_result.info["sum"], round(clr.info["sum"] * 0.5))
+
+    cooltools.api.sample.sample(
+        clr,
+        op.join(request.fspath.dirname, "data/CN.mm9.10000kb.test_sampled.cool"),
+        count=200000000,
+        exact=True,
+    )
+    clr_result = cooler.Cooler(
+        op.join(request.fspath.dirname, "data/CN.mm9.10000kb.test_sampled.cool")
+    )
+    # Test that result matches expectation exactly
+    testing.assert_equal(clr_result.info["sum"], 200000000)
