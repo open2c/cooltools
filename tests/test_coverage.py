a5341aa03f1bbcc1087983f2919602d4f25c333a
Elias Friman <40932182+efriman@users.noreply.github.com>
2022-09-21 20:13:16 +0100

    Added clr_weight_name argument to coverage to calculate balanced coverage (#385)
    * Added `clr_weight_name` argument to coverage to calculate balanced coverage
    * coverage function API change - `store_names` -> `store_prefix`
    * Added tests

--- tests/test_coverage.py
+++ tests/test_coverage.py
@@ -53,3 +53,36 @@ def test_coverage_symmetric_upper(request):
     assert (tot_cov == np.array([3, 1, 2])).all()
     assert clr.info["cis"] == 1
     assert clr.info["sum"] == 3
+    
+def test_balanced_coverage(request):
+    # perform test:
+    clr = cooler.Cooler(op.join(request.fspath.dirname, "data/CN.mm9.1000kb.cool"))
+    cis_cov_weight, tot_cov_weight = cooltools.api.coverage.coverage(
+        clr, ignore_diags=2, chunksize=int(1e7), clr_weight_name="weight"
+    )
+    
+    # Test that mean total balanced coverage is 1.0
+    assert np.nanmean(tot_cov_weight) == 1.0
+    
+    # Generate test matrix with weights
+    bins=pd.DataFrame(
+        [["chr1", 0, 1, 0.5], 
+         ["chr1", 1, 2, 1], 
+         ["chrX", 1, 2, 0.2],
+         ["chrX", 2, 3, np.nan]],
+    columns=["chrom", "start", "end", "weight"],
+    )
+
+    pixels = pd.DataFrame(
+        [[0, 1, 1], [0, 2, 2], [1, 3, 2], [2, 3, 1]], 
+        columns=["bin1_id", "bin2_id", "count"]
+    )
+
+    clr_file = op.join(request.fspath.dirname, "data/test_coverage.cool")
+    cooler.create_cooler(clr_file, bins, pixels)
+    clr = cooler.Cooler(clr_file)
+    cis_cov_weight, tot_cov_weight = cooltools.coverage(clr, ignore_diags=0, store=True, clr_weight_name="weight")
+    assert np.allclose(cis_cov_weight, np.array([0.5, 0.5, 0, np.nan]),
+                       equal_nan=True)
+    assert np.allclose(tot_cov_weight, np.array([0.7, 0.5, 0.2, np.nan]),
+                      equal_nan=True)
