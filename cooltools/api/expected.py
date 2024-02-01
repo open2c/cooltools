074c58b82a47e4b1bc7408070df06c85ee2f68ab
Yaoyx <yxiaoy@umich.edu>
2024-02-01 15:23:45 -0800

    change transforms back to lambda function

--- cooltools/api/expected.py
+++ cooltools/api/expected.py
@@ -974,10 +974,7 @@ def expected_cis(
         # define balanced data transform:
         weight1 = clr_weight_name + "1"
         weight2 = clr_weight_name + "2"
-        # transforms = {"balanced": lambda p: p["count"] * p[weight1] * p[weight2]}
-        def trans_func(p):
-            return p["count"] * p[weight1] * p[weight2]
-        transforms = {"balanced": trans_func}
+        transforms = {"balanced": lambda p: p["count"] * p[weight1] * p[weight2]}
 
     else:
         raise ValueError(
@@ -1114,10 +1111,8 @@ def expected_trans(
         # define balanced data transform:
         weight1 = clr_weight_name + "1"
         weight2 = clr_weight_name + "2"
-        # transforms = {"balanced": lambda p: p["count"] * p[weight1] * p[weight2]}
-        def trans_func(p):
-            return p["count"] * p[weight1] * p[weight2]
-        transforms =  {"balanced": trans_func}
+        transforms = {"balanced": lambda p: p["count"] * p[weight1] * p[weight2]}
+        
     else:
         raise ValueError(
             "cooler is not balanced, or"
