3c4650945b90e207a8b0bdd413728c63bcae9d71
Yaoyx <yxiaoy@umich.edu>
2024-02-02 17:24:35 -0800

    regulate tolerance in test_sample

--- tests/test_sample.py
+++ tests/test_sample.py
@@ -13,25 +13,26 @@ def test_sample(request):
     cooltools.api.sample.sample(
         clr,
         op.join(request.fspath.dirname, "data/CN.mm9.1000kb.test_sampled.cool"),
-        frac=0.1,
+        frac=0.2,
         nproc=3
     )
     clr_result = cooler.Cooler(
         op.join(request.fspath.dirname, "data/CN.mm9.1000kb.test_sampled.cool")
     )
     # Test that deviation from expected total is very small
-    testing.assert_allclose(clr_result.info["sum"], clr.info["sum"] / 10, rtol=1e-2)
+    testing.assert_allclose(clr_result.info["sum"], clr.info["sum"] / 5, rtol=1e-3)
 
     cooltools.api.sample.sample(
         clr,
         op.join(request.fspath.dirname, "data/CN.mm9.1000kb.test_sampled.cool"),
-        count=2000,
+        count=20000000,
+        nproc=3
     )
     clr_result = cooler.Cooler(
         op.join(request.fspath.dirname, "data/CN.mm9.1000kb.test_sampled.cool")
     )
     # Test that deviation from expected total is very small
-    testing.assert_allclose(clr_result.info["sum"], 2000, rtol=1e-2)
+    testing.assert_allclose(clr_result.info["sum"], 20000000, rtol=1e-2)
 
 
 def test_sample_exact(request):
@@ -41,7 +42,7 @@ def test_sample_exact(request):
     cooltools.api.sample.sample(
         clr,
         op.join(request.fspath.dirname, "data/CN.mm9.10000kb.test_sampled.cool"),
-        frac=0.1,
+        frac=0.2,
         exact=True,
         nproc=3
     )
@@ -49,16 +50,17 @@ def test_sample_exact(request):
         op.join(request.fspath.dirname, "data/CN.mm9.10000kb.test_sampled.cool")
     )
     # Test that result matches expectation exactly
-    testing.assert_equal(clr_result.info["sum"], round(clr.info["sum"] * 0.1))
+    testing.assert_equal(clr_result.info["sum"], round(clr.info["sum"] * 0.2))
 
     cooltools.api.sample.sample(
         clr,
         op.join(request.fspath.dirname, "data/CN.mm9.10000kb.test_sampled.cool"),
-        count=2000,
+        count=2000000,
         exact=True,
+        nproc=3
     )
     clr_result = cooler.Cooler(
         op.join(request.fspath.dirname, "data/CN.mm9.10000kb.test_sampled.cool")
     )
     # Test that result matches expectation exactly
-    testing.assert_equal(clr_result.info["sum"], 2000)
+    testing.assert_equal(clr_result.info["sum"], 2000000)
